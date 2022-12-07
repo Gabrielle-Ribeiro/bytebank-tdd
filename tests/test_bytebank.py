ee8122e763fece2d7a15685f0d1a12faa6753dd0
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-12-07 20:21:32 -0300

    Adiciona teste para decrescimo de salário

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -21,3 +21,14 @@ class TestClass:
 
         assert resultado == esperado
 
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada = 100000
+        esperado = 90000
+
+        funcionario_teste = Funcionario('Test', '11/11/2000', entrada)
+        funcionario_teste.decrescimo_salario()
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado
+
+
