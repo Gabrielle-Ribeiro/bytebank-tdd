f4b60d8c58662f9b6038d895ebd3cdc8a2e916bd
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-12-08 13:51:10 -0300

    Refatorando código

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -25,13 +25,13 @@ class Funcionario:
         nome_quebrado = nome_completo.split(' ')
         return nome_quebrado[-1]
 
+    def _eh_socio(self):
+        sobrenomes = ['Bragança', 'Windsor', 'Bourbon', 'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Ptolomeu']
+        return self._salario >= 100000 and self.sobrenome() in sobrenomes
 
     def decrescimo_salario(self):
-        sobrenomes = ['Bragança', 'Windsor', 'Bourbon', 'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Ptolomeu']
-
-        if self._salario >= 100000 and self.sobrenome() in sobrenomes:
-            decrescimo = self._salario * 0.1
-            self._salario = self._salario - decrescimo
+        decrescimo = self._salario * 0.1
+        self._salario = self._salario - decrescimo
 
     def calcular_bonus(self):
         valor = self._salario * 0.1
